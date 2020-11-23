6d5f60ba237b6212d02e746162aab5c16c8701f1
alejandroleon98 <alexleon8898@gmail.com>
2020-11-23 10:18:34 +0100

    fin mi solucion

---
+++ sesion11/ballgame_students/src/game/ApiAdaptadora.java
@@ -0,0 +1,66 @@
+package game;
+
+import java.awt.Point;
+
+import platform.Image2D;
+import platform.android.AndroidAPI;
+import platform.playstation.Playstation5API;
+import platform.windows.WindowsAPI;
+
+public class ApiAdaptadora {
+
+	Platform platform;
+	private AndroidAPI android;
+	private WindowsAPI windows;
+	private Playstation5API playstation;
+
+	public ApiAdaptadora(Platform platform) {
+		this.platform=platform;
+		if (platform == Platform.ANDROID)
+			android = new AndroidAPI();
+		else if (platform == Platform.WINDOWS)
+			windows = new WindowsAPI();
+		else
+			playstation = new Playstation5API();
+	}
+
+	public Image2D loadResource(String name) 
+	{
+		if (platform == Platform.ANDROID)
+			return android.loadResource(name);
+		else if (platform == Platform.WINDOWS)
+			return windows.loadFile(name);
+		else if(platform==Platform.PLAYSTATION)
+			return playstation.loadGraphics(name);
+		else return null;
+	}
+
+	public void draw(int x, int y, Image2D image) 
+	{
+		if (platform == Platform.ANDROID)
+			android.draw(x, y, image);
+		else if (platform == Platform.WINDOWS)
+			windows.paint(x, y, image);
+		else if(platform==Platform.PLAYSTATION)
+			playstation.render(x, y, image);
+	}
+
+	/**
+	 * Devuelve la posición en la que el usuario pulsa con un dedo en la pantalla.
+	 * @return un objeto punto (Point) con la posición donde el usuario ha tocado
+	 *         con el dedo la pantalla
+	 */
+	public Point getTouch() 
+	{
+		if (platform == Platform.ANDROID)
+			return android.getTouch();
+		else if (platform == Platform.WINDOWS)
+			return windows.getMouseClick();
+		else if(platform==Platform.PLAYSTATION)
+			return playstation.getJoystick();
+		else return null;
+	}
+
+	private Point point = new Point(0, 0);
+
+}
--- sesion11/ballgame_students/src/game/BallGame.java
+++ sesion11/ballgame_students/src/game/BallGame.java
@@ -1,11 +1,7 @@
 package game;
 
 import java.awt.Point;
-
 import platform.*;
-import platform.android.*;
-import platform.playstation.*;
-import platform.windows.*;
 
 /* Esta clase (y todo el código que hubiera dentro de este paquete) sería el
  * código del videojuego completo, y es lo que se desea reutilizar en las distintas
@@ -23,9 +19,7 @@ public class BallGame
 	// private Platform platform = Platform.WINDOWS;
 	// private Platform platform = Platform.PLAYSTATION;
 
-	private AndroidAPI android;
-	private WindowsAPI windows;
-	private Playstation5API playstation;
+	private ApiAdaptadora apiAdaptadora;
 
 	public void play() 
 	{
@@ -43,45 +37,25 @@ public class BallGame
 
 	private void setAPI() 
 	{
-		if (platform == Platform.ANDROID)
-			android = new AndroidAPI();
-		else if (platform == Platform.WINDOWS)
-			windows = new WindowsAPI();
-		else
-			playstation = new Playstation5API();
+		apiAdaptadora=new ApiAdaptadora(platform);
 	}
 
 	private Image2D loadImage(String file) 
 	{
 		Image2D image;
-		if (platform == Platform.ANDROID)
-			image = android.loadResource(file);
-		else if (platform == Platform.WINDOWS)
-			image = windows.loadFile(file);
-		else
-			image = playstation.loadGraphics(file);
+		image = apiAdaptadora.loadResource(file);
 		return image;
 	}
 
 	private Point getPosition() 
 	{
 		Point point;
-		if (platform == Platform.ANDROID)
-			point = android.getTouch();
-		else if (platform == Platform.WINDOWS)
-			point = windows.getMouseClick();
-		else
-			point = playstation.getJoystick();
+		point = apiAdaptadora.getTouch();
 		return point;
 	}
 
 	private void drawBall(Image2D image, Point point) 
 	{
-		if (platform == Platform.ANDROID)
-			android.draw(point.x, point.y, image);
-		else if (platform == Platform.WINDOWS)
-			windows.paint(point.x, point.y, image);
-		else
-			playstation.render(point.x, point.y, image);
+		apiAdaptadora.draw(point.x, point.y, image);
 	}
 }
