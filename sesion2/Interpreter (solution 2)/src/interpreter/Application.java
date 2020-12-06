de29177e92b4104eef14644f55d50c1a4ea674a0
alejandroleon98 <alexleon8898@gmail.com>
2020-12-06 18:36:15 +0100

    inicio pr2

---
+++ sesion2/Interpreter (solution 2)/src/interpreter/Application.java
@@ -0,0 +1,40 @@
+package interpreter;
+
+import interpreter.parser.Parser;
+import interpreter.instructions.factories.*;
+
+public class Application 
+{
+	private Parser parser;
+	
+	public static void main(String[] args) throws Exception
+	{
+		new Application().run();
+	}
+	
+	public void run() throws Exception
+	{
+		parser = new Parser();
+		
+		// registra todas las instrucciones de nuestro intérprete junto con el
+		// objeto que se encarga de saber cómo crear cada una
+		registerFactories();
+		
+		Interpreter interpreter = new Interpreter(parser.parse("factorial.txt"));
+		interpreter.run();
+	}
+	
+	private void registerFactories()
+	{
+		parser.registerFactory("PUSH", new PushFactory());
+		parser.registerFactory("ADD", new AddFactory());
+		parser.registerFactory("SUB", new SubFactory());
+		parser.registerFactory("MUL", new MulFactory());
+		parser.registerFactory("JMP", new JumpFactory());
+		parser.registerFactory("JMPG", new JumpIfGreaterThanFactory());
+		parser.registerFactory("LOAD", new LoadFactory());
+		parser.registerFactory("STORE", new StoreFactory());
+		parser.registerFactory("INPUT", new InputFactory());
+		parser.registerFactory("OUTPUT", new OutputFactory());		
+	}	
+}
---
+++ sesion2/Interpreter (solution 3)/src/interpreter/Application.java
@@ -0,0 +1,21 @@
+package interpreter;
+
+import interpreter.parser.Parser;
+
+public class Application 
+{
+	private Parser parser;
+	
+	public static void main(String[] args) throws Exception
+	{
+		new Application().run();
+	}
+	
+	public void run() throws Exception
+	{
+		parser = new Parser();
+		
+		Interpreter interpreter = new Interpreter(parser.parse("factorial.txt"));
+		interpreter.run();
+	}
+}
