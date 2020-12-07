1321080913af2de56a4bb06fe5ada428e381e3ed
alejandroleon98 <alexleon8898@gmail.com>
2020-12-07 21:02:09 +0100

    sesion7hechapormi

---
+++ sesion7/filesystem_students_original/src/outputs/filters/LineFilter.java
@@ -0,0 +1,19 @@
+package outputs.filters;
+
+import java.io.IOException;
+
+import outputs.Output;
+
+public class LineFilter extends AbstractFilter {
+
+	public LineFilter(Output output) {
+		super(output);
+	}
+
+	@Override
+	public void send(char c) throws IOException {
+		if(c!='\t')
+			output.send(c);
+	}
+
+}
