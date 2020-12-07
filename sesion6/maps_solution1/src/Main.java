e7ee5d0c048a453f33f85907e14fc1fc91d03b99
alejandroleon98 <alexleon8898@gmail.com>
2020-12-07 15:17:49 +0100

    empezar7

--- sesion6/maps_solution1/src/Main.java
+++ sesion6/maps_solution1/src/Main.java
@@ -9,6 +9,10 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
+		//Adaptador version de objetos
+		
+		
+		
 		Monument coliseo = new Monument("Coliseo", "Vespasiano", "Piazza del Colosseo, 1, 00184 Roma, Italia");
 		Photo myPhoto = new Photo("Colosseum in Rome, Italy - April 2007", "DAVID ILIFF", new Coordinates(20, 20));
 		Restaurant elGladiador = new Restaurant("La Taverna dei Fori Imperiali", 
